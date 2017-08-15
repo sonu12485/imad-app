4b9b9a1ae67e5c828c17cd977fd1818bcc4cc168
sonu12485 <sonusaikishan@gmail.com>
2017-08-15 23:53:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,10 +23,21 @@ img.onclick = function(){
 
 var button=document.getElementById('counter');
 var span=document.getElementById('count');
-var counter=0;
 
 button.onclick=function(){
-    counter=counter+1;
-    span.innerHTML=counter.toString();
+    
+    var request=new XMLHttpRequest();
+    
+    request.onreadystatechange=function(){
+      if(request.readyState === XMLHttpRequest.DONE){
+          if(request.status == 200){
+              var counter=request.responseText;
+              span.innerHTML=counter.toString();
+          }
+      }  
+    };
+    
+    request.open('GET','http://sonusaikishan.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
 
