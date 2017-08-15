d38b54a7c186cc33235772f32fde32d913e1bd00
sonu12485 <sonusaikishan@gmail.com>
2017-08-15 20:45:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,10 +7,10 @@ var img=document.getElementById("madi");
 var marginLeft=0;
 
 function moveRight(){
-    marginLeft=marginLeft+5;
+    marginLeft=marginLeft+1;
     img.style.marginLeft=marginLeft+"px";
 }
 
 img.onclick = function(){
-  var interval=setInterval(moveRight,50); 
+  var interval=setInterval(moveRight,20); 
 };
