3e619abb5d60f17a39541d6ad47f546811c9de54
sonu12485 <sonusaikishan@gmail.com>
2017-08-20 18:20:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -97,7 +97,7 @@ app.get('/ui/madi.png', function (req, res) {
 var names=[];
 app.get('/submit-name',function(req,res){
    var name=req.query.name;
-   names=push(name);
+   names.push(name);
    res.send(JSON.stringify(names));
 });
 
