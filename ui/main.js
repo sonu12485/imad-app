9a62e133ea9158d564f90d2137f359e3d6480896
sonu12485 <sonusaikishan@gmail.com>
2017-08-20 18:08:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -41,3 +41,42 @@ button.onclick=function(){
     request.send(null);
 };
 
+//this is for name request and response
+
+var submit=document.getElementById('submit-btn');
+submit.onclick=function(){
+  var request=new XMLHttpRequest();
+  request.onreadystatechange=function(){
+    if(request.readystate == XMLHttpRequest.DONE){
+        if(request.status === 200){
+            var names=request.responseText;
+            names=JSON.parse(names);
+            var list='';
+            for(var i=0;i<=names.length;i++){
+                list+='<li>'+names[i]+'</li>';
+            }
+            var ul=document.getElementById('list');
+            ul.innerHTML=list;
+        }
+    }
+  };
+  var nameInput=document.getElementById('name');
+  var name=nameInput.value;
+  request.open('GET','http://sonusaikishan.imad.hasura-app.io/submit-name?name='+name,true);
+  request.send(null);
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
