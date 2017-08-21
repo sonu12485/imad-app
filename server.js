d3d432c1405d7e7a1cd0052f8be44accffd07b80
sonu12485 <sonusaikishan@gmail.com>
2017-08-22 01:32:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -131,7 +131,7 @@ app.get('/submit-name',function(req,res){
 app.get('/articles/:articleName', function(req, res){
     
     var articleName=req.params.articleName;
-    pool.query("SELECT * FROM article WHERE title='"+articleName+"'", function(err,result) {
+    pool.query("SELECT * FROM article WHERE title= $1",[articleName], function(err,result) {
        if(err){
            res.status(500).send(err.toString());
        } else{
