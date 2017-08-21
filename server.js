75c94506161c46f0ff879048967d5318c39c3d27
sonu12485 <sonusaikishan@gmail.com>
2017-08-21 23:43:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,29 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
+//load node-postgres for database
+var Pool=require('pg').Pool;
+//config the database
+var config={
+    user:'sonusaikishan',
+    database:'sonusaikishan',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password:process.env.DB_PASSWORD
+};
+
+var pool=new Pool (config);
+app.get('/test-db',function(req,res){
+   pool.query('SELECT *FROM test',function(err,result){
+     if(err){
+         res.status(500).send(err.toString());
+     }  else{
+         res.send(JSON.stringify(result.rows));
+     }
+   });
+});
+
+
 var app = express();
 app.use(morgan('combined'));
 
