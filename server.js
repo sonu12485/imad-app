eb21db855f8b0e4bd1d0b073946eb10b37af8335
sonu12485 <sonusaikishan@gmail.com>
2017-08-21 23:50:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,18 +13,6 @@ var config={
     password:process.env.DB_PASSWORD
 };
 
-var pool=new Pool (config);
-app.get('/test-db',function(req,res){
-   pool.query('SELECT *FROM test',function(err,result){
-     if(err){
-         res.status(500).send(err.toString());
-     }  else{
-         res.send(JSON.stringify(result.rows));
-     }
-   });
-});
-
-
 var app = express();
 app.use(morgan('combined'));
 
@@ -95,6 +83,17 @@ function createTemplate(data){
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var pool=new Pool (config);
+app.get('/test-db',function(req,res){
+   pool.query('SELECT *FROM test',function(err,result){
+     if(err){
+         res.status(500).send(err.toString());
+     }  else{
+         res.send(JSON.stringify(result.rows));
+     }
+   });
 });
 
 var counter=0;
