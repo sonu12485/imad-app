93a698c3d8623c4e0ca74ca338e6a2091f1b35d9
sonu12485 <sonusaikishan@gmail.com>
2017-08-26 13:18:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,7 +18,7 @@ img.onclick = function(){
 };
 
 */
-
+/*
 //this for the counter 
 
 var button=document.getElementById('counter');
@@ -66,10 +66,31 @@ submit.onclick=function(){
   request.send(null);
 };
 
+*/
 
+//this id to login with username and password
 
-
-
+var submit=document.getElementById('submit-btn');
+submit.onclick=function(){
+  var request=new XMLHttpRequest();
+  request.onreadystatechange=function(){
+    if(request.readyState == XMLHttpRequest.DONE){
+        if(request.status === 200){
+            alert('logged in succesfully');
+        }else if(request.status === 403){
+            alert('invalid username/password');
+        }else if(request.status === 500){
+            alert('server error');
+        }
+    }
+  };
+  var username=document.getElementById('username').value;
+  var password=document.getElementById('password').value;
+  
+  request.open('POST','http://sonusaikishan.imad.hasura-app.io/login',true);
+  request.setRequestHeader('Content-Type','application/json');
+  request.send(JSON.stringify({username:username,password:password}));
+};
 
 
 
